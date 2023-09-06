997cda12899ec0b0ef8890a8b532839520e3ceaa
Lucas Squinca <lucassquinca35@gmail.com>
2023-09-06 16:11:24 -0300

    modificando arquivo para criação da imagem

--- Sprint_4/Docker/Desafio_AWS_4-10/Ex_3/script.py
+++ Sprint_4/Docker/Desafio_AWS_4-10/Ex_3/script.py
@@ -4,6 +4,6 @@ string = input("Digite a sua string: ")
 
 while(string != "quit"):
     sha1_hash = hashlib.sha1(string.encode())
-    print(sha1_hash.hexdigest())
+    print(f'HASH da String: {sha1_hash.hexdigest()}')
 
     string = input("Digite a sua string: ")
