d2845aca1b4f20a7d573970504942a6a344f4fce
Lucas Squinca <lucassquinca35@gmail.com>
2023-11-16 15:25:44 -0300

    Adicionando script sql diagrama dimensional

--- Sprint_9/modelagem-de-dados/tarefa_1/scriptDimensional.sql
+++ Sprint_9/modelagem-de-dados/tarefa_1/scriptDimensional.sql
@@ -1,87 +1,94 @@
 
 
---- Criando view de clientes
-CREATE VIEW dimClientes AS
-SELECT DISTINCT 
-	idCliente as id,
-	nomeCliente as nome,
-	cidadeCliente as cidade,
-	estadoCliente as estado,
-	paisCliente as pais
-FROM Clientes c ;
+CREATE TABLE dimCliente (
+	id INT PRIMARY KEY,
+	nome VARCHAR(100),
+	cidade VARCHAR(40),
+	estado VARCHAR(40),
+	pais VARCHAR(40)
+)
 
-
---- Criando view de tempo
-CREATE VIEW dimTempo AS
+INSERT INTO dimCliente (id, nome, cidade, estado, pais)
 SELECT DISTINCT 
-	dataLocacao,
+	idCliente,
+	nomeCliente,
+	cidadeCliente,
+	estadoCliente,
+	paisCliente
+FROM tb_locacao;
+-------------------------------------
+SELECT * FROM dimCliente;
+-------------------------------------
+CREATE TABLE dimCarros (
+	id INT PRIMARY KEY,
+	classiCarro VARCHAR(50),
+	marca VARCHAR(80),
+	modelo VARCHAR(80),
+	ano INT,
+	tipoCombustivel VARCHAR(20)
+)
+
+INSERT INTO dimCarros (id, classiCarro, marca, modelo, ano, tipoCombustivel)
+SELECT DISTINCT 
+	idCarro,
+	classiCarro,
+	marcaCarro,
+	modeloCarro,
+	anoCarro,
+	tipoCombustivel
+FROM tb_locacao;
+------------------------------
+SELECT * FROM dimCarros ;
+------------------------------
+CREATE TABLE dimVendedores (
+	id INT PRIMARY KEY,
+	nome VARCHAR(15),
+	sexo INT,
+	estado VARCHAR(40)
+)
+
+INSERT INTO dimVendedores (id, nome, sexo, estado)
+SELECT DISTINCT 
+	idVendedor, 
+	nomeVendedor, 
+	sexoVendedor, 
+	estadoVendedor
+FROM tb_locacao;
+-----------------------------------------------------
+SELECT * FROM dimVendedores ;
+-----------------------------------------------------
+
+CREATE TABLE fatoLocacao (
+	id INT PRIMARY KEY,
+	kmCarro INT,
+	qtdDiaria INT,
+    vlrDiaria DECIMAL(18, 2),
+    DataLocacao DATETIME,
+    horaLocacao TIME,
+	DataEntrega DATE,
+	horaEntrega TIME,
+	idCliente INT,
+	idCarro INT,
+	idVendedor INT,
+	FOREIGN KEY (idCliente) REFERENCES dimCliente(id),
+    FOREIGN KEY (idCarro) REFERENCES dimCarros(id),
+    FOREIGN KEY (idVendedor) REFERENCES dimVendedores(id)
+)
+
+INSERT INTO fatoLocacao (id, kmCarro, qtdDiaria, vlrDiaria, DataLocacao, horaLocacao, DataEntrega, horaEntrega, idCliente, idCarro, idVendedor)
+SELECT DISTINCT 
+	idLocacao, 
+	kmCarro, 
+	qtdDiaria, 
+	vlrDiaria, 
+	dataLocacao, 
 	horaLocacao,
-	dataEntrega,
-	horaEntrega
-FROM Locacoes l;
-
-
---- Criando view de carros
-CREATE VIEW dimCarro AS
-SELECT DISTINCT 
-	c.idCarro,
-    c.classiCarro,
-    c.marcaCarro as marca,
-    c.modeloCarro as modelo,
-    c.anoCarro as ano,
-    c.idCombustivel,
-    c2.tipoCombustivel
-FROM Carros c 
-JOIN Combustiveis c2 ON c.idCombustivel = c2.idCombustivel;
-
-
---- Tentativa de criar dimCarro com mais dados -> Descartado
---- CREATE VIEW dimCarro AS
---- SELECT DISTINCT
----    c.idCarro,
----    c.classiCarro,
----    c.marcaCarro as marca,
----    c.modeloCarro as modelo,
----    c.anoCarro as ano,
----    l.kmCarro,
----    c.idCombustivel,
----    c2.tipoCombustivel
---- FROM Carros c
---- JOIN Combustiveis c2 ON c.idCombustivel = c2.idCombustivel
---- JOIN Locacoes l ON c.idCarro = l.idCarro;
-
---- Criando view dos vendedores
-CREATE VIEW dimVendedor AS
-SELECT DISTINCT 
-	v.idVendedor as id,
-	v.nomeVendedor as nome,
-	v.sexoVendedor as sexo,
-	v.estadoVendedor as estado
-FROM Vendedores v ;
-
-SELECT * FROM dimVendedor ;
-
---- Criando view da tabela fato
-
-CREATE VIEW fatoLocacoes AS
-SELECT DISTINCT 
-	idLocacao as id,
+	dataEntrega, 
+	horaEntrega,
 	idCliente,
 	idCarro,
-	idVendedor,
-	kmCarro,
-	qtdDiaria as qtdDiariaLocacao,
-	vlrDiaria as valorDiariaLocacao
-FROM Locacoes l ;
-
---- Consultas ao DW
-
-SELECT * FROM dimClientes;
-
-SELECT * FROM dimTempo;
-
-SELECT * FROM dimCarro ;
-
-SELECT * FROM dimVendedor ;
-
-SELECT * FROM fatoLocacoes ;
+	idVendedor
+FROM tb_locacao
+---------------------------------
+SELECT * from fatoLocacao 
+---------------------------------
